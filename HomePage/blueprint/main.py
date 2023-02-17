98383388bebfb74bd0f34e758cc73b07a8556e9f
hogbal <hogbal31@gmail.com>
2023-02-17 13:27:50 +0900

    Update main

--- HomePage/blueprint/main.py
+++ HomePage/blueprint/main.py
@@ -1,22 +1,79 @@
+import datetime
 from flask import Blueprint, request, jsonify
-from models import story_dashboard
+from models import content_dashboard, content_img, content_tag_list
 
 blue_main = Blueprint("main", __name__, url_prefix="/main")
 
-# @blue_main.route("/content", methods=["POST"])
-# def content():
-#     if(request.method == "POST"):
-#         contents = admin_dashboard.query\
-#             .join(admin_img, admin_dashboard.uid==admin_img.uid, isouter=True)\
-#             .add_columns(admin_img.img)\
-#             .order_by(admin_dashboard.uid)\
-#             .all()
-        
-#         data = [
+@blue_main.route("/content/recruit/<tag>", methods=["POST"])
+def recruit(tag):
+    if(request.method == "POST"):
+        if(tag == 'all'):
+            contentList = content_dashboard.query.filter(content_dashboard.deadline >= datetime.datetime.now()).all()
             
-#         ]
+            data = []
+            
+            for content in contentList:
+                contentImgLen = len(content_img.query.filter(content_img.content_idx==content.idx).all())
+                contentData = {
+                        "idx":content.idx,
+                        "img":f"http://ec2-13-125-123-39.ap-northeast-2.compute.amazonaws.com:5000/util/content/{content.idx}/0" if(contentImgLen != 0) else None,
+                        "title":content.title,
+                        "day":content.day
+                    }
+                data.append(contentData)
+            return data
+        else:
+            contentTagList = content_tag_list.query.filter(content_tag_list.tag==tag).all()
+            
+            data = []
+            
+            for contentTag in contentTagList:
+                content = content_dashboard.query.filter((content_dashboard.idx==contentTag.content_idx) & (content_dashboard.deadline >= datetime.datetime.now())).first()
+                if(content):
+                    contentImgLen = len(content_img.query.filter(content_img.content_idx==content.idx).all())
+                    contentData = {
+                        "idx":content.idx,
+                        "img":f"http://ec2-13-125-123-39.ap-northeast-2.compute.amazonaws.com:5000/util/content/{content.idx}/0" if(contentImgLen != 0) else None,
+                        "title":content.title,
+                        "day":content.day
+                    }
+                    data.append(contentData)
+                
+            return data
         
-#         for i in contents:
-#             print(i)
-        
-#         return "test"
+@blue_main.route("/content/end/<tag>", methods=["POST"])
+def end(tag):
+    if(request.method == "POST"):
+        if(tag == 'all'):
+            contentList = content_dashboard.query.filter(content_dashboard.deadline < datetime.datetime.now()).all()
+            
+            data = []
+            
+            for content in contentList:
+                contentImgLen = len(content_img.query.filter(content_img.content_idx==content.idx).all())
+                contentData = {
+                        "idx":content.idx,
+                        "img":f"http://ec2-13-125-123-39.ap-northeast-2.compute.amazonaws.com:5000/util/content/{content.idx}/0" if(contentImgLen != 0) else None,
+                        "title":content.title,
+                        "day":content.day
+                    }
+                data.append(contentData)
+            return data
+        else:
+            contentTagList = content_tag_list.query.filter(content_tag_list.tag==tag).all()
+            
+            data = []
+            
+            for contentTag in contentTagList:
+                content = content_dashboard.query.filter((content_dashboard.idx==contentTag.content_idx) & (content_dashboard.deadline < datetime.datetime.now())).first()
+                if(content):
+                    contentImgLen = len(content_img.query.filter(content_img.content_idx==content.idx).all())
+                    contentData = {
+                        "idx":content.idx,
+                        "img":f"http://ec2-13-125-123-39.ap-northeast-2.compute.amazonaws.com:5000/util/content/{content.idx}/0" if(contentImgLen != 0) else None,
+                        "title":content.title,
+                        "day":content.day
+                    }
+                    data.append(contentData)
+                
+            return data
